54230575dc2347917d0446fceeaab1aa3675d0e7
Aniket <aniketgupta3625@gmail.com>
2025-10-22 23:31:54 +0530

    frontend link changed

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ import profileRoutes from "./routes/profile.route.js"
 const app=express()
 
 app.use(cors({
-    origin:"https://expense-tracker-frontend-1izd.vercel.app",
+    origin:"https://expense-tracker-frontend-1izd.vercel.app/api",
 }))
 
 // it will parse your json
